1b2597319b893fc1d6f78d563ac0ca33ca45e3d2
Reptile121 <43427766+Reptile121@users.noreply.github.com>
2019-04-01 17:54:50 -0500

    Add files via upload

---
+++ 1.13.2/paperautostart-1.13.2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wget https://papermc.io/api/v1/paper/1.13.2/597/download
+
+mv download paper-597.jar
+
+sudo java -jar paper-597.jar --rev 1.13.2
+
+java -Xms1G -Xmx3G -XX:+UseConcMarkSweepGC -jar paper-597.jar
+
+mv eula.txt eulab.txt
+
+mv eula1.txt eula.txt
+
+rm eulab.txt 
+
+echo Run ./start.sh now
+
